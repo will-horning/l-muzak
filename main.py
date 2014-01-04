e20f765e527bfd23ebd65d4c738d4d1bc17a0c9f
willhorning <will.horning@gmail.com>
2014-01-04 15:57:16 -0500

    added non-determinism, refactored a bunch

--- main.py
+++ main.py
@@ -1,42 +1,36 @@
-import json
+import json, random
 from collections import namedtuple
 from notation import note_freqs
 from pippi import dsp
+from itertools import izip
 
-OUTPATH = 'out'
 NoteSpec = namedtuple('NoteSpec', ['freq', 'env', 'amp', 'duration'])
 
-"""
-A: [[A, B], [A, B, A]]
-"""
+def weighted_choice(a):
+    r = random.uniform(0.0, sum(p[1] for p in a))
+    t = 0
+    for e, w in a:
+        if r <= t + w: return e
+        t += w
 
 def load_grammar(path):
-    g = {}
     with open(path, 'r') as f:
         raw_grammar = json.loads(f.read())
-        for k, v in raw_grammar.iteritems():
-            g[parse_note_spec(k)] = [parse_note_spec(s) for s in v]
-    return g
+        lex = {k: parse_note_spec(v) for k, v in raw_grammar['lexicon'].iteritems()}
+        return raw_grammar['grammar'], lex
+    raise Exception('Failed to load grammar.')
 
-def lsystem_gen(grammar, n_generations, start_string):
-    seq = start_string
-    for i in range(n_generations):
-        next = []
-        for k in seq: 
-            try: next += grammar[k]
-            except KeyError: next += [k]
-        seq = next
-    for ns in seq:
-        if '|' in ns.freq:
-            notes = [dsp.env(dsp.tone(dsp.mstf(ns.duration), note_freqs[f], amp=ns.amp), ns.env) for f in ns.freq.split('|')]
-            yield dsp.mix(notes)
-        else:
-            yield dsp.env(dsp.tone(dsp.mstf(ns.duration), note_freqs[ns.freq], amp=ns.amp), ns.env)
+def lsystem_gen(grammar, lexicon, n_generations, start_string):
+    for i in xrange(n_generations):
+        start_string = "".join([weighted_choice(grammar.get(k, [[k,1]])) for k in start_string])
+    for symbol in start_string:
+        yield lexicon[symbol]
 
 def parse_note_spec(ns_str):
-    rawvals = [s.strip() for s in ns_str.strip().split(',')]
-    return NoteSpec(rawvals[0], rawvals[1], float(rawvals[2]), int(rawvals[3]))
+    ns = NoteSpec(*[s.strip() for s in ns_str.strip().split(',')])
+    out = []
+    for pitch in ns[0].split("|"):
+        out.append(dsp.tone(dsp.mstf(int(ns.duration)), note_freqs[pitch], amp=float(ns.amp)))
+    return dsp.mix(out)
 
-def create_sequence(note_specs):
-    return "".join([dsp.env(dsp.tone(dsp.stf(ns.duration), ns.freq, amp=ns.amp), ns.env) for ns in note_specs])
 
