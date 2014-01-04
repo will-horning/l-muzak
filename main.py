c82cb782fb2ab7a05d38a7f4bcc17daa5066decb
William Horning <will.horning@gmail.com>
2014-01-04 18:58:04 -0500

    major refactor, using pyo instead of pippi now

--- main.py
+++ main.py
@@ -1,36 +1,59 @@
 import json, random
-from collections import namedtuple
 from notation import note_freqs
-from pippi import dsp
 from itertools import izip
+from pyo import *
 
-NoteSpec = namedtuple('NoteSpec', ['freq', 'env', 'amp', 'duration'])
+class LSystem():
+    def __init__(self):
+        self.lex = {"A": self.A, "B": self.B, "C": self.C}
+        self.grammar = {"A": [["A B", 0.5], ["A C", 0.5]], "B" : [["B", 1.0]]}
 
-def weighted_choice(a):
-    r = random.uniform(0.0, sum(p[1] for p in a))
-    t = 0
-    for e, w in a:
-        if r <= t + w: return e
-        t += w
+    def play(self, n_generations, start_string):
+        s = Server().boot()
+        s.start()
+        for i in xrange(n_generations):
+            start_string = " ".join([self.w_choice(self.grammar.get(k, [[k,1.0]])) for k in start_string.split()])
+        print start_string
+        for symbol in start_string.split():
+            self.lex[symbol]()
 
-def load_grammar(path):
-    with open(path, 'r') as f:
-        raw_grammar = json.loads(f.read())
-        lex = {k: parse_note_spec(v) for k, v in raw_grammar['lexicon'].iteritems()}
-        return raw_grammar['grammar'], lex
-    raise Exception('Failed to load grammar.')
+        time.sleep(0.5)
+        s.stop()
+        time.sleep(0.2)
+        s.shutdown()
 
-def lsystem_gen(grammar, lexicon, n_generations, start_string):
-    for i in xrange(n_generations):
-        start_string = "".join([weighted_choice(grammar.get(k, [[k,1]])) for k in start_string])
-    for symbol in start_string:
-        yield lexicon[symbol]
+    def w_choice(self, a):
+        r = random.uniform(0.0, sum(p[1] for p in a))
+        t = 0
+        for e, w in a:
+            if r <= t + w: return e
+            t += w
 
-def parse_note_spec(ns_str):
-    ns = NoteSpec(*[s.strip() for s in ns_str.strip().split(',')])
-    out = []
-    for pitch in ns[0].split("|"):
-        out.append(dsp.tone(dsp.mstf(int(ns.duration)), note_freqs[pitch], amp=float(ns.amp)))
-    return dsp.mix(out)
+    def A(self):
+        env = Adsr(attack=0.1, decay=0.2, sustain=0.5, release=0.1, dur=1, mul=0.5)
+        sin = Sine(freq=[note_freqs['A4'],note_freqs['A4']], mul=env).out()
+        env.play()
+        time.sleep(1.0)
+        env.stop()
+        sin.stop()
+        time.sleep(0.1)
+
+    def B(self):    
+        env = Adsr(attack=0.1, decay=0.2, sustain=0.5, release=0.1, dur=0.5, mul=0.5)
+        sin = Sine(freq=[note_freqs['Bb4'],note_freqs['Bb4']], mul=env).out()
+        env.play()
+        time.sleep(0.5)
+        env.stop()
+        sin.stop()
+        time.sleep(0.1)
+
+    def C(self):
+        env = Adsr(attack=0.1, decay=0.2, sustain=0.5, release=0.1, dur=1, mul=0.5)
+        sin = Sine(freq=[note_freqs['C4'],note_freqs['C4']], mul=env).out()
+        env.play()
+        time.sleep(1.0)
+        env.stop()
+        sin.stop()
+        time.sleep(0.1)
 
 
